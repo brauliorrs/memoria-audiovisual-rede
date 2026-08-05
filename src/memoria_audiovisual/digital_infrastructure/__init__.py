73687a156ed6aaaf48a086e14c70c6e3909f7b3f
brauliorrs <brauliorrs@gmail.com>
2026-08-05 15:33:26 -0300

    feat: exportar infraestrutura experimental de IA

--- src/memoria_audiovisual/digital_infrastructure/__init__.py
+++ src/memoria_audiovisual/digital_infrastructure/__init__.py
@@ -1,10 +1,27 @@
 """Núcleo executável da camada infraestrutura digital."""
 
+from .ai_contracts import (
+    AIEvidenceReference,
+    AIExperimentRecord,
+    AIExperimentRunManifest,
+    AIModelDescriptor,
+)
+from .ai_flags import AIExperimentFlags
+from .ai_runtime import AIExperimentContext, AIShadowRunner
+from .ai_storage import AIExperimentStore
 from .ids import stable_id, version_id
 from .models import EntityRecord, ProvenanceRecord
 from .persistence import JsonlRepository
 
 __all__ = [
+    "AIEvidenceReference",
+    "AIExperimentContext",
+    "AIExperimentFlags",
+    "AIExperimentRecord",
+    "AIExperimentRunManifest",
+    "AIExperimentStore",
+    "AIModelDescriptor",
+    "AIShadowRunner",
     "EntityRecord",
     "JsonlRepository",
     "ProvenanceRecord",
